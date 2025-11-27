82b29da1d062b528542ca0f606b11a7e5712f191
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-11-28 06:14:14 +0700

    nvim devicons

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,9 @@
-require("config.lazy")
+-- set tab
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
@@ -7,21 +12,4 @@ vim.g.loaded_netrwPlugin = 1
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+require("config.lazy")
