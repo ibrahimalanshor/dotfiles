51ce1f11be7dd169855a8f3f838a986689cf8787
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-02 08:58:58 +0700

    vue lsp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,12 +1,35 @@
 local coq = require "coq"
 
-vim.lsp.config('vue_ls', coq.lsp_ensure_capabilities({}))
+local vue_language_server = vim.fn.expand(vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/@vue/language-server")
+
+vim.lsp.config('vue_ls', coq.lsp_ensure_capabilities({
+  init_options = {
+    vue = { hybridMode = false }
+  }
+}))
 vim.lsp.enable('vue_ls')
 
--- vim.lsp.config('ts_ls', coq.lsp_ensure_capabilities({}))
--- vim.lsp.enable('ts_ls')
-
-vim.lsp.config('vtsls', coq.lsp_ensure_capabilities({
-  filetypes = { 'typescript', 'javascript', 'vue' }
+vim.lsp.config('ts_ls', coq.lsp_ensure_capabilities({
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server,
+        languages = { "vue", "javascript", "typescript" },
+      },
+    },
+  },
+  filetypes = {
+    "typescript",
+    "javascript",
+    "javascriptreact",
+    "typescriptreact",
+    "vue",
+  },
 }))
-vim.lsp.enable('vtsls')
+vim.lsp.enable('ts_ls')
+
+-- vim.lsp.config('vtsls', coq.lsp_ensure_capabilities({
+--   filetypes = { 'typescript', 'javascript', 'vue' }
+-- }))
+-- vim.lsp.enable('vtsls')
