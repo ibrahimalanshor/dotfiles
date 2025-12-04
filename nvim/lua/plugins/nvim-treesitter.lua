f8f9be28540c6085341e879402e305290d90abde
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-12-04 11:05:30 +0700

    treesitter

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = { "html", "javascript", "vue", "lua", "markdown" },
+      sync_install = true,
+      highlight = { enable = true },
+      indent = { enable = true }
+    })
+  end
+}
